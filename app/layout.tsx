16d41e048bcba75df4299e6329560d24d527490e
ipulbere <igorpulbere@gmail.com>
2026-02-04 13:30:11 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Template Creator',
+  description: 'AI Website Generator',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
